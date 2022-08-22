1b3e1a1df82c61ca1fa60ac1367b6ef2f00665e5
chuchu275 <111113693+chuchu275@users.noreply.github.com>
2022-08-22 19:53:12 +0900

    go

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('기숙사수용현황분석.csv')
-st.dataframe(data)
+
+col1, col2, col3 = st.columns(3)
+col1.metric("대학수", "70 °F", "1.2 °F")
+col2.metric("국", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
 
 st.title('대학 기숙사 분석')
+
+st.dataframe(data)
 st.write('기숙사현황')
